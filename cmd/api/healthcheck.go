3825284ab803455f1687c5fe2d9a1923442f1dfa
Ankit Mishra <ankit1703@gmail.com>
2026-02-22 18:07:52 +0530

    feat: custom JSON 404 response + error response + JSON refactor

--- cmd/api/healthcheck.go
+++ cmd/api/healthcheck.go
@@ -5,6 +5,14 @@ import (
 )
 
 func (app *application) healthcheckHandler(w http.ResponseWriter, r *http.Request) {
-	w.WriteHeader(http.StatusOK)
-	w.Write([]byte(`{"status":"available"}`))
+	data := map[string]string{
+		"status": "available",
+		"env":    app.config.Env,
+	}
+
+	err := app.writeJSON(w, http.StatusOK, data, nil)
+	if err != nil {
+		app.logger.Println(err)
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+	}
 }
